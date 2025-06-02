f83a3ebde4aef42550ff2f4398f744f828011220
Rafael Schouten <rafaelschouten@gmail.com>
2025-06-02 11:15:26 +0200

    remove extra files

--- src/animal/dget_l_ISO_t.jl
+++ src/animal/dget_l_ISO_t.jl
@@ -4,11 +4,12 @@
 # called from get_lp, get_lj, get_ls
 function dget_l_ISO_t(v_Hl, p, t)
     (; k, l_T, g, f, s_M, v_Hp) = p
-    vH = v_Hl[1]
-    l = v_Hl[2]
+    vH, l = v_Hl
     r = g * (f * s_M - l_T * s_M - l) / l / (f + g) # specific growth rate
     dl = l * r / 3                              # d/dt l
     dvH = f * l^2 * (s_M - l * r / g) - k * vH   # d/dt vH
 
     dvHl = [dvH, dl]
+
+    return dvHl
 end
