53596032f2775ffcef393d68d3ec46b5957eca89
Rafael Schouten <rafaelschouten@gmail.com>
2025-08-28 22:28:20 +1000

    fill out zero variate data

--- test/species/Bos_taurus/mydata_Bos_taurus.jl
+++ test/species/Bos_taurus/mydata_Bos_taurus.jl
@@ -3,18 +3,32 @@
 EstimationData(;
     temperature = 0.0u"K",
     timesincebirth = (
+        Female(Weaning(200.0u"d")),
+        Female(Puberty(307u"d")),
+        Male(Weaning(200"d")),
+        Male(Puberty(295u"d")),
     ),
     timesincefertilisation = (
-    ),
-    length = (
+        Birth(282.5u"d"),
+        Ultimate(32.5*365u"d"),
     ),
     wetweight=(
+        Female(Birth(34.9e3u"g")),
+        Female(Weaning(251.2e3u"g")),
+        Female(Puberty(301.5e3u"g")),
+        Female(Ultimate(650e3u"g")),
+        Male(Birth(37.4e3u"g")),
+        Male(Weaning(264.1e3u"g")),
+        Male(Puberty(374.1e3u"g")),
+        Male(Ultimate(1000e3u"g")),
     ),
-    reproduction = 0.0u"d^-1",
+    # height= TODO withers height
+    reproduction=1/436.7u"d^-1",
     variate = (
+        # TODO get all the real csvs to add here
         MultiVariate(Time(u"d"), FoodIntake(u"kg/d"), MethandEmmisions(u"g/d"), CO2Emissions(u"g/d"), "data.csv"),
         MultiVariate(Time(u"d"), FoodIntake(u"kg/d"), MethandEmmisions(u"g/d"), CO2Emissions(u"g/d"), "otherdata.csv"),
         MultiVariate(Time(u"d"), FoodIntake(u"kg/d"), MethandEmmisions(u"g/d"), CO2Emissions(u"g/d"), "anotherdata.csv"),
-        Univariate(Time(u"d"), Weight(u"kg"), "weigth.csv"),
+        Univariate(Time(u"d"), WetWeight(u"kg"), "wetweigth.csv"),
     ),
 )
