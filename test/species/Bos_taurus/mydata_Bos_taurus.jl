d1e9a28a2536e2f9e661f589e27b39e8a15f0130
Rafael Schouten <rafaelschouten@gmail.com>
2025-08-28 22:03:48 +1000

    add some demonstration lines

--- test/species/Bos_taurus/mydata_Bos_taurus.jl
+++ test/species/Bos_taurus/mydata_Bos_taurus.jl
@@ -1,3 +1,5 @@
+# TODO fill all of this out
+
 EstimationData(;
     temperature = 0.0u"K",
     timesincebirth = (
@@ -8,8 +10,11 @@ EstimationData(;
     ),
     wetweight=(
     ),
-    gestation = 0.0u"d",
     reproduction = 0.0u"d^-1",
     variate = (
+        MultiVariate(Time(u"d"), FoodIntake(u"kg/d"), MethandEmmisions(u"g/d"), CO2Emissions(u"g/d"), "data.csv"),
+        MultiVariate(Time(u"d"), FoodIntake(u"kg/d"), MethandEmmisions(u"g/d"), CO2Emissions(u"g/d"), "otherdata.csv"),
+        MultiVariate(Time(u"d"), FoodIntake(u"kg/d"), MethandEmmisions(u"g/d"), CO2Emissions(u"g/d"), "anotherdata.csv"),
+        Univariate(Time(u"d"), Weight(u"kg"), "weigth.csv"),
     ),
 )
